418bb2feb43305c7aebb1a078afe216369af863f
Will Franklin <will.franklin@guardian.co.uk>
2014-03-06 23:28:08 +0000

    Allow any length phrase

--- tcc.py
+++ tcc.py
@@ -17,10 +17,9 @@ tags = open('%s.tags' % sys.argv[1]).read().split()
 phrase_len = int(sys.argv[2])
 top_len = int(sys.argv[3])
 
-results = tcc(tags, phrase_len)
-for phrase, pos in sorted(results.iteritems(), key=lambda (k, v): len(v))[:top_len]:
-    if len(pos) > 1:
-        print '%s %s' % (len(pos), phrase)
-        for p in pos:
-            print ' '.join(words[p:p+phrase_len])
-        print
+results = tcc(tags, phrase_len).iteritems()
+for phrase, pos in sorted(results, key=lambda (k, v): len(v))[:top_len]:
+    print '%s %s' % (len(pos), phrase)
+    for p in pos:
+        print ' '.join(words[p:p+phrase_len])
+    print
