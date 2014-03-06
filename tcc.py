4a0350dce04a4486103cae88e15dbb48913c7d9a
Will Franklin <will.franklin@guardian.co.uk>
2014-03-06 23:27:43 +0000

    top_len was ignored

--- tcc.py
+++ tcc.py
@@ -18,7 +18,7 @@ phrase_len = int(sys.argv[2])
 top_len = int(sys.argv[3])
 
 results = tcc(tags, phrase_len)
-for phrase, pos in sorted(results.iteritems(), key=lambda (k, v): len(v)):
+for phrase, pos in sorted(results.iteritems(), key=lambda (k, v): len(v))[:top_len]:
     if len(pos) > 1:
         print '%s %s' % (len(pos), phrase)
         for p in pos:
