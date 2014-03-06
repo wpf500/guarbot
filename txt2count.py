69b659fa4d369e1afeb86d57b3620e1a26cea463
Will Franklin <will.franklin@guardian.co.uk>
2014-03-06 23:09:59 +0000

    Added tool to get sentence length distribution

---
+++ txt2count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys, nltk
+from collections import defaultdict
+
+sents = nltk.sent_tokenize(sys.stdin.read())
+counts = defaultdict(int)
+for sent in sents:
+    counts[len(nltk.word_tokenize(sent))] += 1
+
+print 'set nokey'
+print 'plot \'-\' with boxes'
+print
+for length, count in counts.iteritems():
+    print length, count
