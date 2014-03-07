fec78b79a5a3cd892fe8f710fe20fa824a09a295
Will Franklin <will.franklin@guardian.co.uk>
2014-03-07 01:06:30 +0000

    Added text generator HTML

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask, request, render_template
+import cfg
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/cfg')
+def handler():
+    rules = request.args.get('rules', '')
+    base = request.args.get('base', '')
+    if len(rules):
+        cfg_rules = cfg.read_rules(rules.split('\n'))
+        results = [cfg.expand(base, cfg_rules) for i in xrange(10)]
+    else:
+        results = []
+
+    return render_template(
+            'cfg.html',
+            rules=rules,
+            results=results
+        )
