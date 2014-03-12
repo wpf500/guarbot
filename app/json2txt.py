04607d5ca57516b73db7f49e178ab43d706ac090
Will Franklin <will.franklin@guardian.co.uk>
2014-03-12 11:04:31 +0000

    Remove hidden text from body

--- app/json2txt.py
+++ app/json2txt.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import sys, json
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
+
+hidden = ['style', 'script', '[document]', 'head', 'title']
+def get_text(body):
+    def visible(ele):
+        return not (ele.parent.name in hidden or isinstance(ele, Comment))
+    texts = BeautifulSoup(body, 'html5lib').find_all(text=True)
+    return ' '.join(filter(visible, texts))
 
 data = json.loads(sys.stdin.read())
 results = filter(lambda a: 'fields' in a, data['response']['results'])
 texts = ''
 for article in results:
-    body = BeautifulSoup(article['fields']['body'], 'html5lib')
-    texts += ' '.join(body.find_all(text=True)) + ' '
+    texts += get_text(article['fields']['body']) + ' '
 
 print texts.encode('ascii', 'ignore')
