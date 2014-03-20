ccd5056a61607873897cac1b6b277c927b92db32
Will Franklin <will.franklin@guardian.co.uk>
2014-03-20 23:50:25 +0000

    Use CFG read_rules

--- app/rules2json.py
+++ app/rules2json.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python
-import sys, collections, json
-
-rules = collections.defaultdict(list)
-for line in sys.stdin:
-    rule_name, rule = line.strip().split(' ', 1)
-    rules[rule_name].append(rule)
-
-print json.dumps(rules)
+import sys, json, cfg
+print json.dumps(cfg.read_rules(sys.stdin))
