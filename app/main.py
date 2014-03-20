6d7112f2f8d4243107984de56281affbce9977a7
Will Franklin <will.franklin@guardian.co.uk>
2014-03-20 16:12:45 +0000

    Fixed SUBJECT

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ base_rules = cfg.read_rules(open('data/base.rules'))
 
 def get_rules(subject):
     rules = cfg.read_rules(open('data/%s.rules' % subject))
-    rules['SUBJECT'] = subject
+    rules['SUBJECT'] = [subject]
     # add base rules
     for rule_name, rule in base_rules.iteritems():
         if rule_name not in rules:
